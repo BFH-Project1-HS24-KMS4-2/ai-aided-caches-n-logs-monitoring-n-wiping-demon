65d276e31d52bec0d852af49d57a7b12515f4ed9
Janic Scherer <janic.scherer@students.bfh.ch>
2024-10-26 20:20:49 +0200

    set up project and add scheduled task

---
+++ daemon/src/test/java/ch/bfh/tracesentry/daemon/DaemonApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bfh.tracesentry.daemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaemonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
