5c6e4a14e4dfd0d613d900fc90db67497e46ecb6
Scherer Luca Noé <luca.scherer@students.bfh.ch>
2024-10-27 16:34:57 +0100

    wip http

--- daemon/src/main/java/ch/bfh/tracesentry/daemon/facade/DomainLogicController.java
+++ daemon/src/main/java/ch/bfh/tracesentry/daemon/facade/DomainLogicController.java
@@ -1,17 +1,28 @@
 package ch.bfh.tracesentry.daemon.facade;
 
+import ch.bfh.tracesentry.daemon.dto.SearchDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+
 @RestController
 public class DomainLogicController {
 
     @GetMapping("/search")
-    public String search() {
-        return "search";
+    public SearchDTO search() {
+        return new SearchDTO(3,
+                List.of(
+                        "C:\\Users\\user\\Desktop\\test.txt",
+                        "C:\\Users\\user\\Desktop\\test2.txt",
+                        "C:\\Users\\user\\Desktop\\test3.txt",
+                        "/etc/test.txt"
+                )
+        );
     }
 
     @PostMapping("/monitor")
