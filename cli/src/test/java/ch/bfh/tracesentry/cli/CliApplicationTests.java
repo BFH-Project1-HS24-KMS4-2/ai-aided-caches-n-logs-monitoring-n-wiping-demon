27fca06460013d35ce895d66c96bf02d72c6ab7e
Luca Ammann <luca.ammann@students.bfh.ch>
2024-11-01 17:05:11 +0100

    add spring shell test

--- cli/src/test/java/ch/bfh/tracesentry/cli/CliApplicationTests.java
+++ cli/src/test/java/ch/bfh/tracesentry/cli/CliApplicationTests.java
@@ -1,17 +1,45 @@
 package ch.bfh.tracesentry.cli;
 
+import ch.bfh.tracesentry.cli.adapter.DaemonAdapter;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.shell.test.autoconfigure.AutoConfigureShell;
-import org.springframework.shell.test.autoconfigure.AutoConfigureShellTestClient;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.shell.test.ShellAssertions;
+import org.springframework.shell.test.ShellTestClient;
+import org.springframework.shell.test.autoconfigure.ShellTest;
+import org.springframework.test.annotation.DirtiesContext;
+import java.util.concurrent.TimeUnit;
 
-@SpringBootTest
-@AutoConfigureShell
-@AutoConfigureShellTestClient
-public class CliApplicationTests {
+import static org.awaitility.Awaitility.await;
+
+@ShellTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+class CliApplicationTests {
+
+    @Autowired
+    ShellTestClient client;
+
+    @MockBean
+    DaemonAdapter daemonAdapter;
+
     @Test
     void test() {
-        assert(true);
-    }
+        Mockito.when(daemonAdapter.checkStatus()).thenReturn(true);
 
+        ShellTestClient.InteractiveShellSession session = client
+                .interactive()
+                .run();
+
+        await().atMost(2, TimeUnit.SECONDS).untilAsserted(() -> {
+            ShellAssertions.assertThat(session.screen())
+                    .containsText("shell");
+        });
+
+        session.write(session.writeSequence().text("status").carriageReturn().build());
+        await().atMost(2, TimeUnit.SECONDS).untilAsserted(() -> {
+            ShellAssertions.assertThat(session.screen())
+                    .containsText("daemon is running");
+        });
+    }
 }
