9ac5dc485e7bb77e3efc389dbb9391a6cc4e90cd
Scherer Luca Noé <luca.scherer@students.bfh.ch>
2024-11-18 03:05:04 +0100

    wip add mode parameter with validation

--- cli/src/main/java/ch/bfh/tracesentry/cli/command/SearchCommands.java
+++ cli/src/main/java/ch/bfh/tracesentry/cli/command/SearchCommands.java
@@ -1,22 +1,21 @@
 package ch.bfh.tracesentry.cli.command;
 
 import ch.bfh.tracesentry.cli.adapter.DaemonAdapter;
-import ch.bfh.tracesentry.cli.command.validation.SearchModeProvider;
+import ch.bfh.tracesentry.cli.command.validation.annotations.ValidSearchMode;
 import ch.bfh.tracesentry.lib.dto.SearchResponseDTO;
 import jakarta.validation.constraints.NotBlank;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.shell.command.annotation.ExceptionResolver;
 import org.springframework.shell.standard.ShellCommandGroup;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
 
-import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Objects;
-import java.util.regex.Pattern;
 
 @ShellComponent
 @ShellCommandGroup("Search Commands")
@@ -29,25 +28,30 @@ public class SearchCommands {
         this.daemonAdapter = daemonAdapter;
     }
 
-
     @ShellMethod(key = "search", value = "Search for files in a given path.")
     @SuppressWarnings("unused")
-    public String search(@ShellOption String path) {
+    public String search(
+            @ShellOption @NotBlank String path,
+            @ShellOption @ValidSearchMode String mode
+    ) {
         if (!daemonAdapter.checkStatus()) return "daemon is not running";
-
         try {
             Path absolutePath = Paths.get(path).toAbsolutePath();
             ResponseEntity<SearchResponseDTO> searchResponse = daemonAdapter.search(absolutePath);
             var body = Objects.requireNonNull(searchResponse.getBody());
-            List<String> foundRelativePaths = body
-                    .getFiles()
-                    .stream()
-                    .map(absoluteFilePath -> absolutePath.relativize(Paths.get(absoluteFilePath)).toString())
-                    .toList();
+            List<String> foundRelativePaths = parseFoundPaths(body.getFiles(), absolutePath);
             String outputPaths = String.join("\n", foundRelativePaths);
             return "Listing " + body.getNumberOfFiles() + " files in " + absolutePath + ":\n" + outputPaths;
+        } catch (IllegalArgumentException e) {
+            return e.getMessage();
         } catch (Exception ignored) {
             return "An error occurred while searching for files";
         }
+    }
+
+    private List<String> parseFoundPaths(List<String> foundPaths, Path absolutePath) {
+        return foundPaths.stream()
+                .map(absoluteFilePath -> absolutePath.relativize(Paths.get(absoluteFilePath)).toString())
+                .toList();
     }
 }
---
+++ cli/src/main/java/ch/bfh/tracesentry/cli/config/ShellValidationConfig.java
@@ -0,0 +1,31 @@
+package ch.bfh.tracesentry.cli.config;
+
+import jakarta.validation.ConstraintViolation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.shell.ParameterValidationException;
+import org.springframework.shell.command.CommandExceptionResolver;
+import org.springframework.shell.command.CommandHandlingResult;
+
+
+@Configuration
+public class ShellValidationConfig {
+    static class CustomExceptionResolver implements CommandExceptionResolver {
+
+        @Override
+        public CommandHandlingResult resolve(Exception e) {
+            if (e instanceof ParameterValidationException) {
+                var violations = ((ParameterValidationException) e).getConstraintViolations();
+                var message = violations.stream()
+                        .map(ConstraintViolation::getMessage)
+                        .reduce("", (a, b) -> a + b + "\n");
+                return CommandHandlingResult.of(message);
+            }
+            return null;
+        }
+    }
+    @Bean
+    CustomExceptionResolver customExceptionResolver() {
+        return new CustomExceptionResolver();
+    }
+}
