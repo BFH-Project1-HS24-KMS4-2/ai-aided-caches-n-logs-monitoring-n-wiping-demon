7fe67301b9787839cce59ea32526553dea5f16d0
Janic Scherer <janic.scherer@students.bfh.ch>
2024-12-07 01:48:59 +0100

    test change detection

--- daemon/src/test/java/ch/bfh/tracesentry/daemon/scheduling/MonitoringSchedulerITest.java
+++ daemon/src/test/java/ch/bfh/tracesentry/daemon/scheduling/MonitoringSchedulerITest.java
@@ -19,6 +19,10 @@ import java.nio.file.Path;
 import java.time.LocalDate;
 import java.util.List;
 
+/**
+ * Test class for {@link MonitoringScheduler}.
+ * This covers the entire execution flow of the scheduler. Therefore, the tree creation is also tested here.
+ */
 @DataJpaTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class MonitoringSchedulerITest {
@@ -42,14 +46,7 @@ public class MonitoringSchedulerITest {
     @Test
     public void testCreateSnapshot(@TempDir Path rootDir) throws IOException {
         // given
-        Path cacheFile = rootDir.resolve("cache.txt");
-        Files.createFile(cacheFile);
-        Files.writeString(cacheFile, "12345");
-        Path logsDir = rootDir.resolve("logs");
-        Files.createDirectory(logsDir);
-        Path someLogs = logsDir.resolve("someLogs.txt");
-        Files.createFile(someLogs);
-        Files.writeString(someLogs, "some logs");
+        var tempDirStructure = createTempDirStructure(rootDir);
 
         var createdAt = LocalDate.of(2024, 11, 1);
         monitoredPathRepository.save(
@@ -67,8 +64,8 @@ public class MonitoringSchedulerITest {
         var nodes = nodeRepository.findAll();
 
         var rootDirNode = nodes.stream().filter(node -> node.getParent() == null).findFirst().orElseThrow();
-        var cacheFileNode = nodes.stream().filter(node -> node.getPath().equals(cacheFile.toString())).findFirst().orElseThrow();
-        var logsDirNode = nodes.stream().filter(node -> node.getPath().equals(logsDir.toString())).findFirst().orElseThrow();
+        var cacheFileNode = nodes.stream().filter(node -> node.getPath().equals(tempDirStructure.cacheFile.toString())).findFirst().orElseThrow();
+        var logsDirNode = nodes.stream().filter(node -> node.getPath().equals(tempDirStructure.logsDir.toString())).findFirst().orElseThrow();
 
         // then
 
@@ -93,15 +90,96 @@ public class MonitoringSchedulerITest {
         Assertions.assertEquals(cacheFileNode.getParent(), rootDirNode);
         Assertions.assertEquals(cacheFileNode.getSnapshot(), snapshot);
         Assertions.assertEquals(cacheFileNode.getChildren(), List.of());
-        Assertions.assertEquals(cacheFileNode.getPath(), cacheFile.toString());
+        Assertions.assertEquals(cacheFileNode.getPath(), tempDirStructure.cacheFile.toString());
         Assertions.assertTrue(cacheFileNode.isHasChanged());
         Assertions.assertFalse(cacheFileNode.isDeletedInNextSnapshot());
 
         Assertions.assertEquals(logsDirNode.getParent(), rootDirNode);
         Assertions.assertEquals(logsDirNode.getSnapshot(), snapshot);
         Assertions.assertEquals(logsDirNode.getChildren(), List.of());
-        Assertions.assertEquals(logsDirNode.getPath(), logsDir.toString());
+        Assertions.assertEquals(logsDirNode.getPath(), tempDirStructure.logsDir.toString());
         Assertions.assertTrue(logsDirNode.isHasChanged());
         Assertions.assertFalse(logsDirNode.isDeletedInNextSnapshot());
+    }
+
+    @Test
+    public void testChangeDetection(@TempDir Path rootDir) throws IOException {
+        // given
+        var tempDirStructure = createTempDirStructure(rootDir);
+
+        var createdAt = LocalDate.of(2024, 11, 1);
+        var monitoredPath = monitoredPathRepository.save(
+                new MonitoredPath()
+                        .path(rootDir.toString())
+                        .mode(SearchMode.CACHE)
+                        .noSubdirs(false)
+                        .createdAt(createdAt));
+
+        // when
+        monitoringScheduler.createSnapshots();
+
+        // change
+        Files.writeString(tempDirStructure.cacheFile, "change");
+        monitoringScheduler.createSnapshots();
+
+        var snapshots = snapshotRepository.findAll();
+        var currentSnapshotOptional = snapshotRepository.findFirstByMonitoredPathIdOrderByTimestampDesc(monitoredPath.getId());
+        if (currentSnapshotOptional.isEmpty()) {
+            throw new IllegalStateException("Snapshot not found");
+        }
+        var currentSnapshot = currentSnapshotOptional.get();
+        var nodes = nodeRepository.findAllBySnapshotId(currentSnapshot.getId());
+
+        var rootDirNode = nodes.stream().filter(node -> node.getParent() == null).findFirst().orElseThrow();
+        var cacheFileNode = nodes.stream().filter(node -> node.getPath().equals(tempDirStructure.cacheFile.toString())).findFirst().orElseThrow();
+        var logsDirNode = nodes.stream().filter(node -> node.getPath().equals(tempDirStructure.logsDir.toString())).findFirst().orElseThrow();
+
+        // then
+
+        // monitored path creation
+        Assertions.assertEquals(1, monitoredPathRepository.findAll().size());
+
+        // snapshot creation
+        Assertions.assertEquals(2, snapshots.size());
+        Assertions.assertEquals(rootDir.toString(), currentSnapshot.getMonitoredPath().getPath());
+        Assertions.assertEquals(createdAt, currentSnapshot.getMonitoredPath().getCreatedAt());
+
+        // tree creation
+        Assertions.assertEquals(3, nodes.size());
+
+        Assertions.assertNull(rootDirNode.getParent());
+        Assertions.assertEquals(rootDirNode.getSnapshot(), currentSnapshot);
+        Assertions.assertEquals(rootDirNode.getChildren(), List.of(cacheFileNode, logsDirNode));
+        Assertions.assertEquals(rootDirNode.getPath(), rootDir.toString());
+        Assertions.assertTrue(rootDirNode.isHasChanged()); // root dir has changed
+        Assertions.assertFalse(rootDirNode.isDeletedInNextSnapshot());
+
+        Assertions.assertEquals(cacheFileNode.getParent(), rootDirNode);
+        Assertions.assertEquals(cacheFileNode.getSnapshot(), currentSnapshot);
+        Assertions.assertEquals(cacheFileNode.getChildren(), List.of());
+        Assertions.assertEquals(cacheFileNode.getPath(), tempDirStructure.cacheFile.toString());
+        Assertions.assertTrue(cacheFileNode.isHasChanged()); // cache file has changed
+        Assertions.assertFalse(cacheFileNode.isDeletedInNextSnapshot());
+
+        Assertions.assertEquals(logsDirNode.getParent(), rootDirNode);
+        Assertions.assertEquals(logsDirNode.getSnapshot(), currentSnapshot);
+        Assertions.assertEquals(logsDirNode.getChildren(), List.of());
+        Assertions.assertEquals(logsDirNode.getPath(), tempDirStructure.logsDir.toString());
+        Assertions.assertFalse(logsDirNode.isHasChanged()); // logs dir has not changed
+        Assertions.assertFalse(logsDirNode.isDeletedInNextSnapshot());
+    }
+
+    private record TempDirStructure(Path cacheFile, Path logsDir, Path someLogs) { }
+
+    private static TempDirStructure createTempDirStructure(Path rootDir) throws IOException {
+        Path cacheFile = rootDir.resolve("cache.txt");
+        Files.createFile(cacheFile);
+        Files.writeString(cacheFile, "12345");
+        Path logsDir = rootDir.resolve("logs");
+        Files.createDirectory(logsDir);
+        Path someLogs = logsDir.resolve("someLogs.txt");
+        Files.createFile(someLogs);
+        Files.writeString(someLogs, "some logs");
+        return new TempDirStructure(cacheFile, logsDir, someLogs);
     }
 }
