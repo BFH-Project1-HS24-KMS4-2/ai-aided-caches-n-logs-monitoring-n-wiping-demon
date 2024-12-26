5b74a9026fcbf67465da4db3fcc7a0a470433119
Janic Scherer <janic.scherer@students.bfh.ch>
2024-12-26 22:21:35 +0100

    use spring annotation

--- daemon/src/main/java/ch/bfh/tracesentry/daemon/scheduling/MonitoringScheduler.java
+++ daemon/src/main/java/ch/bfh/tracesentry/daemon/scheduling/MonitoringScheduler.java
@@ -8,12 +8,12 @@ import ch.bfh.tracesentry.daemon.domain.model.Snapshot;
 import ch.bfh.tracesentry.daemon.domain.repo.MonitoredPathRepository;
 import ch.bfh.tracesentry.daemon.domain.repo.NodeRepository;
 import ch.bfh.tracesentry.daemon.domain.repo.SnapshotRepository;
-import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.io.File;
 import java.sql.Timestamp;
